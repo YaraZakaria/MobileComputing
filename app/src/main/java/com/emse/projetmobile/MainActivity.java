44121048a122b9a7df2fbd352f4334f8717bb704
Yara Zakaria <yarazakaria91@gmail.com>
2018-12-18 13:15:05 +0100

    final application

---
+++ app/src/main/java/com/emse/projetmobile/MainActivity.java
@@ -0,0 +1,58 @@
+package com.emse.projetmobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonNext;
+    private RecyclerView recyclerView;
+    private NameListAdapter nameListAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nameListAdapter=new NameListAdapter();
+        initViews();
+        initList();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nameListAdapter.updateData(DataManager.getInstance().getNameList());
+    }
+
+    private void initViews(){
+        //textViewName = (TextView)findViewById(R.id.main_textview);
+        buttonNext = (Button) findViewById(R.id.button_next);
+        buttonNext.setOnClickListener(this);
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+
+    }
+    private void initList(){
+        final LinearLayoutManager linearLayoutManager= new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(nameListAdapter);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_next:
+                Intent intent = new Intent(this, FormActivity.class);
+                startActivityForResult(intent,1);
+                break;
+        }
+
+    }
+}
